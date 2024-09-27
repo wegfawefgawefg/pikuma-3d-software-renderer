e3e3370c6d70eee0200a2793c45fb168e410bb02
wegfawefgawefg <668es218pur@gmail.com>
2024-09-27 01:59:16 -0500

    stdbool use

--- src/globals.h
+++ src/globals.h
@@ -1,9 +1,11 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <stdbool.h>
+
 #define RENDER_WIDTH 240 * 2
 #define RENDER_HEIGHT 160 * 2
-#define FRAME_LIMITING 0
+#define FRAME_LIMITING true
 #define TARGET_FPS 120
 #define TARGET_FRAME_TIME (1000 / TARGET_FPS)
 
